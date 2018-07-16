15321e2d5dd36aefe53297c62b45190be796021c
Caner Turkmen <turkmen.ac@gmail.com>
2018-07-16 12:22:31 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,7 @@ import numpy
 from distutils.extension import Extension
 
 setup(
-    ext_modules=cythonize([Extension("em", ["em.pyx"], include_dirs=[numpy.get_include()],
-                                        libraries=["m"],
-                                        extra_compile_args = ["-O3", "-ffast-math", "-march=native", "-fopenmp"],
-                                        extra_link_args=['-fopenmp']),
-                             Extension("model.c.c_uv_exp", ["model/c/c_uv_exp.pyx"], include_dirs=[numpy.get_include()],
+    ext_modules=cythonize([Extension("model.c.c_uv_exp", ["model/c/c_uv_exp.pyx"], include_dirs=[numpy.get_include()],
                                        libraries=["m"],
                                        language="c++",
                                        extra_compile_args=["-O3", "-ffast-math", "-march=native", "-fopenmp"],
