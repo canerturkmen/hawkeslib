716e80c27906a0daf9e7dec900c594084ee5dc40
Caner Turkmen <turkmen.ac@gmail.com>
2018-09-25 22:27:48 +0300

    rewired mv_exp, made corrections

--- hawkeslib/model/c/c_mv_exp.pyx
+++ hawkeslib/model/c/c_mv_exp.pyx
@@ -2,6 +2,8 @@ import cython
 import numpy as np
 cimport numpy as cnp
 
+from .c_mv_samp import mv_sample_branching
+
 cnp.import_array()
 
 cdef extern from "math.h":
@@ -16,11 +18,14 @@ cdef extern from "stdlib.h":
 cdef double uu() nogil:
     return <double> rand() / RAND_MAX
 
-cdef extern from "<vector>" namespace "std":
-    cdef cppclass vector[T]:
-        void push_back(T&) nogil except+
-        size_t size()
-        T& operator[](size_t)
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def mv_exp_sample_branching(double T,
+                            cnp.ndarray[ndim=1, dtype=cnp.float64_t] mu,
+                            cnp.ndarray[ndim=2, dtype=cnp.float64_t] A,
+                            double theta):
+    # wraps the new generic sampler for backward compatibility
+    return mv_sample_branching(T, mu, A, theta, 0, 0, 0, 0)
 
 
 @cython.boundscheck(False)
@@ -77,86 +82,6 @@ def mv_exp_ll(cnp.ndarray[ndim=1, dtype=cnp.float64_t] t,
             d[ci] = 0.
 
     return lJ + -np.sum(mu * T) - np.sum(A.T.dot(F))
-
-
-@cython.cdivision(True)
-@cython.boundscheck(False)
-@cython.wraparound(False)
-def _get_mv_offspring(double t, cnp.ndarray[ndim=1, dtype=cnp.float64_t] Acp, double theta, double T):
-    """
-    :param t: time of parent
-    :param Acp: A[c_parent, :]
-    :param K: int, number of processes
-    """
-    cdef:
-        int Nk, K = Acp.shape[0]
-        vector[cnp.float64_t] tos
-        vector[long] cos
-        int i, j
-        long k
-        double tt
-
-    for k in range(K):
-        Nk = np.random.poisson(Acp[k])
-        for j in range(Nk):
-            tt = -log(uu()) / theta + t
-            tos.push_back(<cnp.float64_t> tt)
-            cos.push_back(k)
-
-    cdef cnp.ndarray[cnp.float64_t] tres = np.empty(tos.size(), dtype=np.float)
-    cdef cnp.ndarray[long] cres = np.empty(cos.size(), dtype=np.int)
-    for i in range(tres.shape[0]):
-        tres[i] = tos[i]
-        cres[i] = cos[i]
-
-    return tres[tres < T], cres[tres < T]
-
-@cython.boundscheck(False)
-@cython.wraparound(False)
-def mv_exp_sample_branching(double T,
-                            cnp.ndarray[ndim=1, dtype=cnp.float64_t] mu,
-                            cnp.ndarray[ndim=2, dtype=cnp.float64_t] A, double theta):
-    """
-    Implements a branching sampler for a univariate exponential HP, taking advantage of the
-    cluster process representation.
-    """
-    cdef:
-        cnp.ndarray[cnp.float64_t] P = np.array([])
-        cnp.ndarray[long] C = np.array([], dtype=np.int)
-
-        int Nk_0, K = mu.shape[0]
-        int i, k
-        cnp.ndarray[cnp.float64_t] curr_P = np.array([])
-        cnp.ndarray[long] curr_C = np.array([], dtype=np.int)
-
-    for k in range(K):
-        Nk0 = np.random.poisson(mu[k] * T)
-
-        Pk0 = np.random.rand(Nk0) * T
-        Ck0 = np.ones(Pk0.shape[0], dtype=np.int) * k
-
-        curr_P = np.concatenate([curr_P, Pk0])
-        curr_C = np.concatenate([curr_C, Ck0])
-
-    while curr_P.shape[0] > 0:
-        P = np.concatenate([P, curr_P])
-        C = np.concatenate([C, curr_C])
-
-        os_P = []  # offspring timestamps
-        os_C = []  # offspring marks
-
-        for i in range(len(curr_P)):
-            ci = curr_C[i]
-            tres, cres = _get_mv_offspring(curr_P[i], A[ci, :], theta, T)
-            os_P.append(tres)
-            os_C.append(cres)
-
-        curr_P = np.concatenate(os_P)
-        curr_C = np.concatenate(os_C)
-
-    six = np.argsort(P, kind="mergesort")
-
-    return P[six], C[six]
 
 
 @cython.cdivision(True)
